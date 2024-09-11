3c621fe421052a261b8fc1fe8af11c8b1374e879
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2024-09-11 08:49:03 -0700

    [848] [Time Beats: 84.21%] [Memory Beats: 36.84%] - LeetPush

---
+++ 848-Shifting-Letters.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def shiftingLetters(self, s, shifts):
+        """
+        :type s: str
+        :type shifts: List[int]
+        :rtype: str
+        """
+        res = ""
+        shift = 0
+        for i in range(len(s)-1, -1, -1):
+            shift += shifts[i]
+            res += chr((ord(s[i])+shift-ord('a')) % 26 + ord('a'))
+        return res[::-1]
