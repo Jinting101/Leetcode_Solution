0c33351b37f3f3de80b94c316e6077722e1531fe
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2024-09-15 21:39:46 -0700

    [539] [Time Beats: 5.33%] [Memory Beats: 22.13%] - LeetPush

--- DCP-09-24/539-Minimum-Time-Difference.py
+++ DCP-09-24/539-Minimum-Time-Difference.py
@@ -6,7 +6,7 @@ class Solution(object):
         \\\
 
         # version 1
-        
+
         # def find_td(t1, t2):
         #     h1, m1, h2, m2 = t1[:2], t1[-2:], t2[:2], t2[-2:]
         #     m1t, m2t = int(h1)*60+int(m1), int(h2)*60+int(m2)
@@ -25,49 +25,21 @@ class Solution(object):
 
         # version 2
 
-        # If there is any two of the same return 0
-        if len(timePoints) != len(set(timePoints)):
-            return 0
+        # Convert times to minutes
+        minutes = []
+        for time in timePoints:
+            h, m = map(int, time.split(':'))
+            minutes.append(h * 60 + m)
         
-        # First instince is to order them 
-        sorted_time = sorted(timePoints)
-        # add first time to the end for measurement
-        sorted_time.append(sorted_time[0])
-
-        def subtract_time(time_1, time_2):
-            \\\
-            time_1: (str) lesser time e.g. 00:13
-            time_2: (str) greater time eg. 01:02
-            \\\
-            time_2_hour = int(time_2.split(':')[0])
-            time_1_hour = int(time_1.split(':')[0])
-
-            time_2_min = int(time_2.split(':')[-1])
-            time_1_min = int(time_1.split(':')[-1])
-            # different hours
-            # if 13:10 - 09:50 can be read as 12:70 - 09:50
-            if time_2_min < time_1_min:
-                time_2_hour = time_2_hour - 1
-                time_2_min = time_2_min + 60
-            
-            # if time 2 is across the 24 hour mark, convert it to 24
-            # this should only happen at the end of the cycle.
-            if time_2_hour < time_1_hour:
-                time_2_hour = time_2_hour + 24
-                
-            # caluclate hour and minute difference
-            hour_diff = time_2_hour - time_1_hour
-            min_diff = time_2_min - time_1_min
-            
-            # Add total minutes
-            total_diff = hour_diff*60 + min_diff
-
-            return total_diff
+        # Sort times in ascending order
+        minutes.sort()
         
-        min_diff_seen = 60*24
-        for i, time in enumerate(sorted_time[:-1]):
-            difference = subtract_time(time, sorted_time[i + 1])
-            if difference < min_diff_seen:
-                min_diff_seen = difference
+        # Find minimum difference across adjacent elements
+        min_diff = float('inf')
+        for i in range(len(minutes) - 1):
+            min_diff = min(min_diff, minutes[i + 1] - minutes[i])
         
-        return min_diff_seen
+        # Consider the circular difference between last and first element
+        min_diff = min(min_diff, 24 * 60 - minutes[-1] + minutes[0])
+        
+        return min_diff
