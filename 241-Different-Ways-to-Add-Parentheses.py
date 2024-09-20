eb1754f750e3dd7687371cb6402211a77ec561c3
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2024-09-19 17:29:01 -0700

    [241] [Time Beats: 99.58%] [Memory Beats: 43.51%] - LeetPush

--- 241-Different-Ways-to-Add-Parentheses.py
+++ 241-Different-Ways-to-Add-Parentheses.py
@@ -1,29 +1,50 @@
 class Solution(object):
     def diffWaysToCompute(self, expression, memo={}):
         \\\
+        Returns all possible results of computing the expression 
+        by splitting it at each operator.
+
         :type expression: str
         :rtype: List[int]
         \\\
+        # Base case: expression is a number
         if expression.isdigit():
             return [int(expression)]
-        if expression in memo:
-            return memo[expression] 
-        res = []
-        for i in range(len(expression)):
-            if expression[i] in \-+*\:
-                res1 = self.diffWaysToCompute(expression[:i])
-                res2 = self.diffWaysToCompute(expression[i+1:])
-                for j in res1:
-                    for k in res2:
-                        res.append(self.helper(j, k, expression[i]))
-        memo[expression] = res
-        return res
 
-    def helper(self, m, n, op):
+        # Memoization
+        if expression in memo:
+            return memo[expression]
+
+        results = []
+        for i, char in enumerate(expression):
+            # Check if character is an operator
+            if char in \-+*\:
+                # Recursively compute left and right expressions
+                left_results = self.diffWaysToCompute(expression[:i], memo)
+                right_results = self.diffWaysToCompute(expression[i+1:], memo)
+
+                # Combine results using the operator
+                for left in left_results:
+                    for right in right_results:
+                        results.append(self.compute(left, right, char))
+
+        # Store results in memo
+        memo[expression] = results
+        return results
+
+    def compute(self, m, n, op):
+        \\\
+        Helper function to compute the result of two numbers 
+        with the given operator.
+
+        :type m: int
+        :type n: int
+        :type op: str
+        :rtype: int
+        \\\
         if op == \+\:
-            return m+n
+            return m + n
         elif op == \-\:
-            return m-n
+            return m - n
         else:
-            return m*n
-    
+            return m * n
