9b6723c145feaf9cdd0e1fb5eb8997b60e4bf848
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2024-10-19 15:39:45 -0700

    [2326] [Time Beats: 100.00%] [Memory Beats: 43.29%] - LeetPush

--- 2326-Spiral-Matrix-IV.py
+++ 2326-Spiral-Matrix-IV.py
@@ -5,39 +5,70 @@
 #         self.next = next
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        # matrix = [[-1] * n for _ in range(m)]
+        # flag = 'r'
+        # row, col = 0, 0
+
+        # while head:
+        #     matrix[row][col] = head.val
+
+        #     # Determine the next direction
+        #     if flag == 'r' and (col + 1 >= n or matrix[row][col + 1] != -1):
+        #         flag = 'd'
+        #     elif flag == 'd' and (row + 1 >= m or matrix[row + 1][col] != -1):
+        #         flag = 'l'
+        #     elif flag == 'l' and (col - 1 < 0 or matrix[row][col - 1] != -1):
+        #         flag = 'u'
+        #     elif flag == 'u' and (row - 1 < 0 or matrix[row - 1][col] != -1):
+        #         flag = 'r'
+
+        #     # Move based on the current direction
+        #     if flag == 'r':
+        #         col += 1
+        #     elif flag == 'd':
+        #         row += 1
+        #     elif flag == 'l':
+        #         col -= 1
+        #     elif flag == 'u':
+        #         row -= 1
+
+        #     # If the next position is already filled, break
+        #     if row >= m or col >= n or row < 0 or col < 0 or matrix[row][col] != -1:
+        #         break
+
+        #     head = head.next
+
+        # return matrix
+
+        # Initialize matrix with -1
         matrix = [[-1] * n for _ in range(m)]
-        flag = 'r'
+
+        # Initialize direction flags
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
+        dir_index = 0
+        
+        # Initialize current position
         row, col = 0, 0
-
+        
         while head:
+            # Fill current cell with value from linked list
             matrix[row][col] = head.val
-
-            # Determine the next direction
-            if flag == 'r' and (col + 1 >= n or matrix[row][col + 1] != -1):
-                flag = 'd'
-            elif flag == 'd' and (row + 1 >= m or matrix[row + 1][col] != -1):
-                flag = 'l'
-            elif flag == 'l' and (col - 1 < 0 or matrix[row][col - 1] != -1):
-                flag = 'u'
-            elif flag == 'u' and (row - 1 < 0 or matrix[row - 1][col] != -1):
-                flag = 'r'
-
-            # Move based on the current direction
-            if flag == 'r':
-                col += 1
-            elif flag == 'd':
-                row += 1
-            elif flag == 'l':
-                col -= 1
-            elif flag == 'u':
-                row -= 1
-
-            # If the next position is already filled, break
-            if row >= m or col >= n or row < 0 or col < 0 or matrix[row][col] != -1:
-                break
-
+            
+            # Determine next direction
+            next_row, next_col = row + directions[dir_index][0], col + directions[dir_index][1]
+            
+            # Check if next position is valid
+            if (0 <= next_row < m and 0 <= next_col < n and 
+                matrix[next_row][next_col] == -1):
+                row, col = next_row, next_col
+            else:
+                # Change direction
+                dir_index = (dir_index + 1) % 4
+                row, col = row + directions[dir_index][0], col + directions[dir_index][1]
+            
+            # Move to next node in linked list
             head = head.next
-
+        
         return matrix
 
 # Initialize an empty matrix of size m x n with all values set to -1.
