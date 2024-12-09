0763a1778b5f0a66077e60e147f99c0b45614713
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2024-12-09 14:23:37 -0800

    [3152] [Time Beats: 67.40%] [Memory Beats: 34.88%] - LeetPush

---
+++ 3152-Special-Array-II.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        n = len(nums)
+        cum = [0] * n
+        ans = []
+
+        for i in range(1, n):
+            cum[i] = cum[i - 1]
+            if nums[i] % 2 == nums[i - 1] % 2:
+                cum[i] += 1
+
+        for q in queries:
+            x, y = q
+            falseCount = cum[y] - cum[x]
+            if falseCount and x != y:
+                ans.append(False)
+            else:
+                ans.append(True)
+
+        return ans
+
+
+        
+        res = []
+        for st, ed in queries:
+            if st==ed:
+                res.append(True)
+            else:
+                temp = True
+                for i in range(st, ed):
+                    if nums[i] % 2 == nums[i+1] % 2:
+                        temp = False
+                        break
+                res.append(temp)
+        return res
