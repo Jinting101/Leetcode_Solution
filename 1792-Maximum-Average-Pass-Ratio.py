5e31f60cd9697cfda767d04016471ed9f5359ca3
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2024-12-15 14:57:20 -0800

    [1792] [Time Beats: 54.14%] [Memory Beats: 37.50%] - LeetPush

--- 1792-Maximum-Average-Pass-Ratio.py
+++ 1792-Maximum-Average-Pass-Ratio.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         q = []
+        res, l = 0, 0
         for x, y in classes:
+            res += x/y
+            l += 1
             z = (-((x+1)/(y+1)-x/y), x, y)
             heapq.heappush(q, z)
         for i in range(extraStudents):
             cur, x, y = heapq.heappop(q)
+            res -= x / y
             x, y = x + 1, y + 1
+            res += x / y
             z = (-((x+1)/(y+1)-x/y), x, y)
             heapq.heappush(q, z)
-        res, l = 0, 0
-        while q:
-            cur, x, y = heapq.heappop(q)
-            res += x/y
-            l += 1
         return res / l
