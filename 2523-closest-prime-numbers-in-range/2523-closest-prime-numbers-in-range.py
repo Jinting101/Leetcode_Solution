9355103dfc65ab966e063bccf476fcefce065f0c
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-03-07 14:35:19 -0800

    Time: 1568 ms (47.28%), Space: 35.1 MB (18.21%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -1,32 +1,55 @@
 class Solution:
-    def closestPrimes(self, left: int, right: int) -> List[int]:
-        if left <= 2 and right >= 3:
-            return [2, 3]
-        def check(x):
-            prime = True
-            for i in range(2, x):
-                if x % i == 0:
-                    prime = False
-                    break
-            return prime
+    def closestPrimes(self, left: int, right: int) -> list[int]:
+        sieve = [True] * (right + 1)
+        sieve[0] = sieve[1] = False
         
-        prev, x = 0, left
-        res = []
-        while x <= right:
-            if check(x):
-                res.append(x)
-                if x != prev and x - prev == 2:
-                    return [prev, x]
-                prev = x
-                x += 2
-            else:
-                x += 1
-        if len(res) <= 1:
+        for i in range(2, int(right**0.5) + 1):
+            if sieve[i]:
+                for j in range(i*i, right + 1, i):
+                    sieve[j] = False
+        
+        primes = [i for i in range(left, right + 1) if sieve[i]]
+        
+        if len(primes) < 2:
             return [-1, -1]
-        y = min(res[i]-res[i-1] for i in range(1, len(res)))
-        for i in range(1, len(res)):
-            if res[i] - res[i-1] == y:
-                return [res[i-1], res[i]]
-
         
-        # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, ... 59, 61, 65]
+        min_gap = float('inf')
+        result = [-1, -1]
+        
+        for i in range(1, len(primes)):
+            gap = primes[i] - primes[i-1]
+            if gap < min_gap:
+                min_gap = gap
+                result = [primes[i-1], primes[i]]
+        
+        return result
+
+# class Solution:
+#     def closestPrimes(self, left: int, right: int) -> List[int]:
+#         if left <= 2 and right >= 3:
+#             return [2, 3]
+#         def check(x):
+#             prime = True
+#             for i in range(2, x):
+#                 if x % i == 0:
+#                     prime = False
+#                     break
+#             return prime
+        
+#         prev, x = 0, left
+#         res = []
+#         while x <= right:
+#             if check(x):
+#                 res.append(x)
+#                 if x != prev and x - prev == 2:
+#                     return [prev, x]
+#                 prev = x
+#                 x += 2
+#             else:
+#                 x += 1
+#         if len(res) <= 1:
+#             return [-1, -1]
+#         y = min(res[i]-res[i-1] for i in range(1, len(res)))
+#         for i in range(1, len(res)):
+#             if res[i] - res[i-1] == y:
+#                 return [res[i-1], res[i]]
