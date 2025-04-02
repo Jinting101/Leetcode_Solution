50655866c634f022e1edff9fa1ea394f19a33a05
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-04-02 12:16:37 -0700

    Time: 7 ms (65.59%), Space: 17.9 MB (19.2%) - LeetHub

---
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        leftMax = [0] * n
+        rightMax = [0] * n
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], nums[i - 1])
+            rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - i])
+        res = 0
+        for j in range(1, n - 1):
+            res = max(res, (leftMax[j] - nums[j]) * rightMax[j])
+        return res
+
+
+
+        
+        n = len(nums)
+        # If we fix k, then the value of the triplet is maximized when nums[i]−nums[j] takes the maximum value. We can use imax to maintain the maximum value of nums[i] and dmax to maintain the maximum value of nums[i]−nums[j]. During the enumeration of k, update dmax and imax.
+        res, imax, dmax = 0, 0, 0
+        for k in range(n):
+            res = max(res, dmax * nums[k])
+            dmax = max(dmax, imax - nums[k])
+            imax = max(imax, nums[k])
+        return res
