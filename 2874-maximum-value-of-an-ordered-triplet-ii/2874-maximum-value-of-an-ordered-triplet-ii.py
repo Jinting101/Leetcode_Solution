48e214506df50376205b4b35f4f6251a3fcab48b
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-04-03 12:02:56 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
@@ -1,16 +1,29 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         n = len(nums)
-        leftmax = [0]*n
-        rightmax = [0]*n
-        for i in range(1, n):
-            leftmax[i] = max(leftmax[i-1], nums[i-1])
-        for i in range(n-2, 0, -1):
-            rightmax[i] = max(rightmax[i+1], nums[i+1])
-        
-        res = 0
-        for i in range(1, n-1):
-            x = nums[i]
-            res = max(res, (leftmax[i] - x) * rightmax[i])
-
+        res, imax, jmin, update = 0, 0, float('inf'), False
+        for k in range(n):
+            res = max(res, (imax-jmin) * nums[k])
+            if imax < nums[k]:
+                update = True
+            else:
+                update = False
+            imax = max(imax, nums[k])
+            jmin = min(jmin, nums[k])
+            if update:
+                jmin = float('inf')
         return res
+
+
+        n = len(nums)
+        leftMax = [0] * n
+        rightMax = [0] * n
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], nums[i - 1])
+            rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - i])
+        res = 0
+        for j in range(1, n - 1):
+            res = max(res, (leftMax[j] - nums[j]) * rightMax[j])
+        return res
+
+        
