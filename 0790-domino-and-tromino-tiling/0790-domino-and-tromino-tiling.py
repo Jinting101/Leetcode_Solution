a96ba500ab6f0e50c76f231104fe181a3f2beff6
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-05-05 15:46:19 -0700

    Time: 0 ms (100%), Space: 18 MB (40.08%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        dp = [0] * max((n+1), 4)
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+        for i in range(4, n+1):
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % (10**9+7)
+        return dp[n]
