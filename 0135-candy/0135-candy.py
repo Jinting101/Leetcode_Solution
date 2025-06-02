eeb6fda5b39fc990c6de3dfbcc7265875d9e377b
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-06-02 13:43:15 -0700

    Time: 15 ms (69.29%), Space: 19.8 MB (68.8%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,35 +1,21 @@
-class Solution:
-    def candy(self, ratings: List[int]) -> int:
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
         n = len(ratings)
-        if n == 1:
-            return 1
-        l, r = min(ratings), max(ratings)
-        dic = {i:[] for i in range(l, r+1)}
-        res = [0]*(n+1)
-        for i,x in enumerate(ratings):
-            dic[x].append(i)
-        for cur_rating in dic:
-            for ind in dic[cur_rating]:
-                if cur_rating == l:
-                    res[ind] = 1
-                elif ind == 0:
-                    if ratings[ind] <= ratings[ind+1]:
-                        res[ind] = 1
-                    else:
-                        res[ind] = res[ind+1] + 1
-                elif ind == n-1:
-                    if ratings[ind] <= ratings[ind-1]:
-                        res[ind] = 1
-                    else:
-                        res[ind] = res[ind-1] + 1
-                else:
-                    if ratings[ind] > ratings[ind-1] and ratings[ind] > ratings[ind+1]:
-                        res[ind] = max(res[ind-1], res[ind+1]) + 1
-                    elif ratings[ind] > ratings[ind-1]:
-                        res[ind] = res[ind-1] + 1
-                    elif ratings[ind] > ratings[ind+1]:
-                        res[ind] = res[ind+1] + 1
-                    else:
-                        res[ind] = 1
-        return sum(res)
+        candies = [1] * n  # Step 1: Give each child 1 candy initially
 
+        # Step 2: Left to Right pass
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Step 3: Right to Left pass
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        # Step 4: Return total candies
+        return sum(candies)
