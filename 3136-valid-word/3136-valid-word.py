c84f1e291eb89f63498e48720a42bffdcb4c8b2c
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-07-15 14:18:31 -0700

    Time: 0 ms (100%), Space: 17.8 MB (34.23%) - LeetHub

--- 3136-valid-word/3136-valid-word.py
+++ 3136-valid-word/3136-valid-word.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValid(self, word: str) -> bool:
         vowels = ['a', 'e', 'i', 'o', 'u']
-        cond1, cond2, cond3 = False, False, False
+        cond1, cond2 = False, False
         for x in word:
             if x.isdigit():
-                cond1 = True
+                continue
             elif x.isalpha():
                 if x.lower() in vowels:
-                    cond2 = True
+                    cond1 = True
                 else:
-                    cond3 = True
+                    cond2 = True
             else:
                 return False
-        return cond1 and cond2 and cond3
+        return cond1 and cond2 and len(word) > 2
             
