1883bb4585d8c9a6433c395c745803c935c74510
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-10-13 13:47:04 -0700

    Time: 3 ms (89.42%), Space: 18 MB (7.47%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,8 +1,18 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        res = []
-        for x in words:
-            if res and Counter(x) == Counter(res[-1]):
-                continue
-            res.append(x)
+        def getFreq(word):
+            res = [0] * 26
+            for char in word:
+                res[ord(char) - ord('a')]+=1
+            return res
+        
+        preAnagram = getFreq(words[0])
+        res = [words[0]]
+
+        for i in range(1, len(words)):
+            curAnagram = getFreq(words[i])
+            if curAnagram != preAnagram:
+                res.append(words[i])
+                preAnagram = curAnagram
+        
         return res
