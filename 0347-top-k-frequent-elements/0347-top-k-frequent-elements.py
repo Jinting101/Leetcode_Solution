37aa926ea4cae6413b932d5c7d069817cc1b8495
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-10-29 11:18:48 -0700

    Time: 4 ms (68.19%), Space: 21.1 MB (80.7%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,9 +3,18 @@ class Solution:
         dic = {}
         for x in nums:
             dic[x] = dic.get(x, 0) + 1
-        lst = [(-x,y) for y,x in dic.items()]
+        lst = [(-y, x) for x,y in dic.items()]
         heapq.heapify(lst)
         res = []
-        for i in range(k):
-            res.append(heapq.heappop(lst)[1])
+        for _ in range(k):
+            nf, num = heapq.heappop(lst)
+            res.append(num)
         return res
+
+        # {1:3, 2:2, 3:1}
+
+        # [(-3, 1), (-2, 2), (-1, 3)]
+
+        # O(n)
+        # O(n log k)
+        
