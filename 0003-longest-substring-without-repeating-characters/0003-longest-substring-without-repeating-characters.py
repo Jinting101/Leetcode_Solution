2b7135f5064ab8031ad7c17e71621be0eb9645c5
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-12-04 20:53:57 -0800

    Time: 15 ms (80.93%), Space: 17.7 MB (81.51%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,13 +5,10 @@ class Solution:
         res = 0
         for r in range(len(s)):
             cur = s[r]
-            if cur not in seen:
-                seen.add(cur)
-            else:
-                while cur in seen:
-                    if s[l] in seen:
-                        seen.remove(s[l])
-                        l += 1
-                seen.add(cur)
+            while cur in seen:
+                # if s[l] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(cur)
             res = max(res, r-l+1)
         return res
