a2931d7beae0cefcd9c2d07347e5e5d159528295
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-12-06 15:09:11 -0800

    Time: 11 ms (94.99%), Space: 18.4 MB (6.09%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def serialize(node):
+            if not node:
+                return "N"
+            return f"({node.val},{serialize(node.left)},{serialize(node.right)})"
+        
+        rootSerialized = serialize(root)
+        subRootSerialized = serialize(subRoot)
+        return subRootSerialized in rootSerialized
+
+        
+        def isSameTree(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+        def helper(p, q):
+            if not p:
+                return False
+            if isSameTree(p, q):
+                return True
+            return helper(p.left, q) or helper(p.right, q)
+
+        return helper(root, subRoot)
