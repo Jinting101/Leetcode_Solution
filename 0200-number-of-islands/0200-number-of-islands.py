297fcf01410ab7808359f53b25dcc01b10a4d901
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2025-12-08 22:55:29 -0800

    Time: 258 ms (40.64%), Space: 20.3 MB (42.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,12 +4,27 @@ class Solution:
         visited = [[0]*n for _ in range(m)]
         directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         res = 0
+        
+        def bfs(r, c):
+            q = deque()
+            visited.add((r, c))
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if 0 <= r < m and 0 <= c < n and not visited[nr][nc] and grid[r][c] == "1":
+                        q.append((r, c))
+                        visited.add((r, c))
 
         def dfs(r, c):
+            visited[r][c] = 1
             for i,j in directions:
                 nr, nc = r + i, c + j
                 if 0 <= nr < m and 0 <= nc < n and not visited[nr][nc] and grid[nr][nc] == "1":
-                    visited[nr][nc] = 1
                     dfs(nr, nc)
         
         for i in range(m):
