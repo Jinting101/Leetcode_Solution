57e32a697f91ad186090614277aaf58de90cc0e9
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2026-01-16 22:01:36 -0800

    Time: 93 ms (79.55%), Space: 66.4 MB (99.18%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,31 +1,23 @@
 class TimeMap:
 
     def __init__(self):
-        self.timemap = {}
+        self.dic = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.timemap:
-            self.timemap[key] = {-1:[]}
-        self.timemap[key][-1].append(timestamp)
-        self.timemap[key][timestamp] = value
+        if not self.dic[key]:
+            self.dic[key].append([])
+            self.dic[key].append([])
+        self.dic[key][0].append(timestamp)
+        self.dic[key][1].append(value)
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.timemap:
+        if key not in self.dic:
             return ""
-        if timestamp in self.timemap[key]:
-            return self.timemap[key][timestamp]
-        idx = bisect.bisect_right(self.timemap[key][-1], timestamp)
+        idx = bisect.bisect_right(self.dic[key][0], timestamp)
         if idx == 0:
             return ""
-        return self.timemap[key][self.timemap[key][-1][idx-1]]
+        return self.dic[key][1][idx-1]
 
-        
-
-        
-# 1 4 5
-# {'foo':{-1:[1, 4], 1:'bar', 4:'bar2'}}
-# {'foo':{-1:[5], 5:'bar'}}
-# get('foo', 1) - 5 !< 1
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
