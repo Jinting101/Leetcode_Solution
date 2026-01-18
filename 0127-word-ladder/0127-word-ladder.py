346c67e03f7ce67c4a0555ea131834fd9ae435d2
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2026-01-18 15:55:03 -0800

    Time: 6355 ms (7.87%), Space: 20.8 MB (41.33%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,58 +2,43 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        
-        def check(x, y):
-            dif = 0
-            for i, cur in enumerate(x):
-                if cur == y[i]:
-                    continue
-                dif += 1
-                if dif > 1:
-                    return
-            adj[x].append(y)
-            adj[y].append(x)
-
+            
         adj = defaultdict(list)
         n = len(wordList)
+
+        def transform(x, y):
+            dif = 0
+            for a,b in zip(x, y):
+                if a != b:
+                    dif += 1
+                    if dif > 1:
+                        return False
+            adj[x].append(y)
+            adj[y].append(x)
+        
         for i in range(n):
             for j in range(i+1, n):
-                x, y = wordList[i], wordList[j]
-                check(x, y)
+                transform(wordList[i], wordList[j])
         
         if beginWord not in wordList:
             for x in wordList:
-                check(beginWord, x)
-        
-        q = deque([(beginWord, [])])
-        visited = set([beginWord])
-        res = 0
-        while q:
-            res += 1
-            l = len(q)
-            for i in range(l):
-                cur, path = q.popleft()
-                if cur == endWord:
-                    return res
-                for nei in adj[cur]:
-                    if nei not in visited:
-                        q.append((nei, path + [nei]))
-                        visited.add(nei)  
-        return 0
-        
+                transform(beginWord, x)
+
+        res = 1
         q = deque([beginWord])
-        visited = set([beginWord])
-        res = 0
-        print(adj)
+        visited = set()
+        visited.add(beginWord)
+
         while q:
             l = len(q)
-            for i in range(l):
-                cur = q.popleft()
-                if cur == endWord:
+            for _ in range(l):
+                word = q.popleft()
+                if word == endWord:
                     return res
-                for nei in adj[cur]:
+                for nei in adj[word]:
                     if nei not in visited:
-                        q.append(nei)
                         visited.add(nei)
+                        q.append(nei)
             res += 1
+        
         return 0
