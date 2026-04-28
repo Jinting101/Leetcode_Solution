ddab5d555a0aecf9e286938fcf229f3b09afa454
Jinting101 <100323530+Jinting101@users.noreply.github.com>
2026-04-27 23:10:46 -0700

    Time: 93 ms (98.63%), Space: 43.2 MB (9.59%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -1,39 +1,30 @@
 class Solution:
-    def minOperations(self, grid, x):
-        values = sorted([val for row in grid for val in row])
-        
-        diff = [abs(val - values[0]) % x for val in values]
-        if any(d != 0 for d in diff):
-            return -1
-        
-        median = values[len(values) // 2]
-        operations = sum(abs(val - median) // x for val in values)
-        
-        return operations
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        n, m = len(grid), len(grid[0])
+        N = n * m
+        freq = [0] * 10001
+        mn = grid[0][0]
+        mx = mn
 
-        
-        # Create a list to store all the numbers from the grid
-        nums_array = []
-        result = 0
-
-        # Flatten the grid into nums_array
         for row in grid:
-            for num in row:
-                nums_array.append(num)
+            for c in row:
+                if (c - grid[0][0]) % x != 0: return -1
+                freq[c] += 1
+                mn = min(mn, c)
+                mx = max(mx, c)
 
-        # Sort nums_array in non-decreasing order to easily find the median
-        nums_array.sort()
+        target = (N + 1) // 2
+        acc = 0
+        median = mn
 
-        length = len(nums_array)
-        # Store the median element as the final common value
-        final_common_number = nums_array[length // 2]
+        for i in range(mn, mx + 1, x):
+            acc += freq[i]
+            if acc >= target:
+                median = i
+                break
 
-        # Iterate through each number in nums_array
-        for number in nums_array:
-            # If the remainder when divided by x is different, return -1
-            if number % x != final_common_number % x:
-                return -1
-            # Add the number of operations required to make the current number equal to final_common_number
-            result += abs(final_common_number - number) // x
+        ops = 0
+        for i in range(mn, mx + 1, x):
+            ops += abs(i - median) // x * freq[i]
 
-        return result
+        return ops
